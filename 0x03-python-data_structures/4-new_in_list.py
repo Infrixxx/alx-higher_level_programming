ad716cfa31ed96a8c03588e62dd29820e36d76af
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-08-14 19:46:30 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     """Replace an element at specific index without modifying the whole list"""
-    if idx < 0 or idx >= (len(my_list):
+    if idx < 0 or idx >= (len(my_list)):
         return my_list
 
     copy = [x for x in my_list]
