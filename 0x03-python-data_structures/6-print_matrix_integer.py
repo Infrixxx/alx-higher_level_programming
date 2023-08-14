dd7ad7a79e9086196bad2ea4e1b2a25a784970af
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-08-14 22:19:35 +0200

    Into the matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     """Function that prints a matrix of integers"""
     for a in range(len(matrix)):    #from 0 to length of elements in list
         for b in range(len(matrix[a])):    #from 0 to lenght of elements in sub list
-            if b != -1:
+            if b != 2:
                 print(" ", end='')  #creates space when it's not first element
             print("{:d}".format(matrix[a][b]), end='')
         print() #creates newline
