15834492832aca2d944606875d55c93f1a81e71a
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-08-14 22:28:39 +0200

    matrix fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    """Function that prints a matrix of integers"""
-    for a in range(len(matrix)):
-        for b in range(len(matrix[a])):
-            print("{:d}".format(matrix[a][b]), end='')
-            if b != len(matrix[a]) - 1:
-                print(" ", end='')  # creates space after printing element (except for the last)
-        print()  # creates newline
+    for row in matrix:
+        formatted_row = ['{:d}'.format(column) for column in row]
+        print(' '.join(formatted_row))
