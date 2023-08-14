6a60b17c2a9c353064640c5cac487319c511a347
Boitumelo <bcrachoshi@gmail.com>
2023-08-14 21:58:09 +0200

    fix to file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """Function that prints a matrix of integers"""
-    for a in matrix:
-        for b in a:
+    for a in range(len(matrix)):    #from 0 to length of elements in list
+        for b in range(len(matrix[a])):    #from 0 to lenght of elements in sub list
             if b != 0:
-                print(" ", end='')
+                print(" ", end='')  #creates space when it's not first element
             print("{:d}".format(b), end='')
-        print()
+        print() #creates newline
