e4db9a6c6e9793551df37613652f3fbbd2275c26
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-08-14 20:45:23 +0200

    Into the Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     for n in matrix:
-        print("{:d}\n".format(n))
+        for element in n:
+            print("{:d}".format(element), end=" ")
+        print()  # Move to the next line after printing each sublist
