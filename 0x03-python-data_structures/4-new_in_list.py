bdaa9ec320eb9fdd777f41a2959123925b70113d
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-08-14 19:47:29 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Replace an element at specific index without modifying the whole list"""
     if idx < 0 or idx >= (len(my_list)):
         return my_list
 
