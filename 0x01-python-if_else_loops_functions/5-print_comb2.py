888958401ddf4f566c1089cccd5f541a6582e1b8
Boitumelo <bcrachoshi@gmail.com>
2023-08-16 17:47:49 +0200

    mandatory tasks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-n = 0;
+n = 0
 while n <= 99:
     if n < 99:
         a = n // 10
-        b = n%10
-        print("{:d}{:d}".format(a,b),end=', ')
+        b = n % 10
+        print("{:d}{:d}".format(a, b), end=', ')
         n += 1
     else:
         print("{:d}".format(n))
