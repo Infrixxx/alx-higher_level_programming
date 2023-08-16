8820d1239444b6d4f1813a13940cc4871e6328eb
Boitumelo <bcrachoshi@gmail.com>
2023-08-16 18:17:29 +0200

    mandatory tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for a in range(9):
-    for b in range(a + 1, 9):
+for a in range(10):
+    for b in range(a + 1, 10):
+        if a!=8:
             print("{:d}{:d}".format(a, b), end=', ')
-print('89')
+        else:
+            print("{:d}{:d}".format(a, b))
