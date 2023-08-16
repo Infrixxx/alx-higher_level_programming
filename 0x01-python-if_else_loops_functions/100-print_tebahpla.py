5c1ae1f3f55b603c86e5750db328f889c9f0fc8d
Boitumelo <bcrachoshi@gmail.com>
2023-08-16 18:49:07 +0200

    mandatory tasks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        n = chr(i)
-    else:
-        n = chr(i-32)
-    print("{}".format(n), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format(c - (32 if c % 2 != 0 else 0)), end='')
