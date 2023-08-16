8f372661a12778a2389ddf38c87f69151b55c503
Boitumelo <bcrachoshi@gmail.com>
2023-08-16 18:18:21 +0200

    mandatory tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for a in range(10):
     for b in range(a + 1, 10):
-        if a!=8:
+        if a != 8:
             print("{:d}{:d}".format(a, b), end=', ')
         else:
             print("{:d}{:d}".format(a, b))
